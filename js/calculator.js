debbdde77f59bf33e43d98208b94be1aa7d148e1
Aaron <aaron.devleague@gmail.com>
2016-09-10 11:35:02 -1000

    wip

---
+++ js/calculator.js
@@ -0,0 +1,123 @@
+/**
+ * Declare a function named `calculatorModule`
+ * this function will have two private variables declared inside of it.
+ * @variable PRIVATE { Number } `memory`
+ * @variable PRIVATE { Number } `total`
+ * @return {object} `calculator` object that can be used
+ */
+function calculatorModule(){
+
+
+  let memory = 0;
+  let total = 0;
+  let  calculator = {};
+
+
+
+  //function validation(x){
+
+  /**
+   * sets the `total` to the number passed in
+   * @param  { Number } x
+   * @return { Number }    current total
+   */
+  calculator.load = function(x){
+    if(typeof x !== 'number'){
+      throw new Error('wrong') ;
+    }
+    total  = x;
+    console.log(total);
+    return  total;
+    };
+
+  /**
+   * Return the value of `total`
+   * @return { Number }
+   */
+   calculator.getTotal = function(){
+    if(total === undefined){
+      total = 0;
+    }return total;
+   };
+
+  /**
+   * Sums the value passed in with `total`
+   * @param { Number } x
+   */
+
+   calculator.add = function(x){
+    if(typeof x !== 'number'){
+      throw new Error('wrong') ;
+    }
+    total += x;
+    console.log(total);
+    return total;
+   };
+  /**
+   * Subtracts the value passed in from `total`
+   * @param  { Number } x
+   */
+   calculator.subtract = function(x){
+    if(typeof x !== 'number'){
+      throw new Error('wrong') ;
+    }
+    total -= x;
+    return total;
+   };
+
+  /**
+   * Multiplies the value by `total`
+   * @param  { Number } x
+   */
+   calculator.multiply = function(x){
+    if(typeof x !== 'number'){
+      throw new Error('wrong') ;
+    }
+    total *= x;
+    return total;
+   };
+
+  /**
+   * Divides the value passing in by `total`
+   * @param  { Number } x
+   */
+   calculator.divide = function(x){
+    if(typeof x !== 'number'){
+      throw new Error('wrong') ;
+    }
+    total /= x;
+    return total;
+   };
+
+
+  /**
+   * Return the value stored at `memory`
+   * @return { Number }
+   */
+   calculator.recallMemory = function(x){
+    return memory;
+   };
+
+  /**
+   * Stores the value of `total` to `memory`
+   */
+   calculator.saveMemory = function(x){
+    memory = total;
+    return this.memory;
+   };
+
+  /**
+   * Clear the value stored at `memory`
+   */
+calculator.clearMemory = function(x){
+  memory = 0;
+  return memory;
+  };
+  /**
+   * Validation
+   */
+
+
+return calculator;
+}
+
